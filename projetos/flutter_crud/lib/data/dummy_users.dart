379cbc8a41b6c3c9261458bc0fb47cb2d85e54e4
wodejunior <wodejunior@gmail.com>
2021-04-03 23:42:55 -0300

    push.01

---
+++ projetos/flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User (
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_960_720.png',
+  ),
+  '2': const User (
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2012/04/13/21/07/user-33638_1280.png',
+  ),
+  '3': const User (
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@zymail.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_960_720.png',
+  ),
+  '4': const User (
+      id: '4',
+      name: 'Guilherme',
+      email: 'guigui@abcmail.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2012/04/13/21/07/user-33638_1280.png',
+  ),
+};
