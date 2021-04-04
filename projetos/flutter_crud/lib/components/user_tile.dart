a779fc77fb733c505b69e28b8ef03a52af24c141
wodejunior <wodejunior@gmail.com>
2021-04-04 11:53:48 -0300

    Codigo Final

--- projetos/flutter_crud/lib/components/user_tile.dart
+++ projetos/flutter_crud/lib/components/user_tile.dart
@@ -1,16 +1,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
 import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class UserTile extends StatelessWidget {
   final User user;
-
   const UserTile(this.user);
 
   @override
   Widget build(BuildContext context) {
-
     final avatar = user.avatarUrl == null || user.avatarUrl.isEmpty
       ? CircleAvatar( child: Icon(Icons.person) )
       : CircleAvatar( backgroundImage: NetworkImage(user.avatarUrl) );
@@ -35,7 +35,33 @@ class UserTile extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.delete),
               color: Colors.red,
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                  context: context,
+                    builder: (ctx) => AlertDialog(
+                      title: Text('Excluir Usuário'),
+                      content: Text('Confirma a Exclusão?'),
+                      actions: <Widget>[
+                        FlatButton(
+                          child: Text('Não'),
+                          onPressed: () {
+                            Navigator.of(context).pop(false);
+                          },
+                          ),
+                        FlatButton(
+                          child: Text('Sim'),
+                          onPressed: () {
+                            Navigator.of(context).pop(true);
+                          },
+                        ),
+                      ],
+                    ),
+                ).then( (confimed) {
+                  if(confimed){
+                    Provider.of<Users>(context, listen: false).remove(user);
+                  }
+                });
+              },
             ),
           ]
         ),
--- projetos/flutter_crud/lib/views/user_form.dart
+++ projetos/flutter_crud/lib/views/user_form.dart
@@ -1,82 +1,116 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
 import 'package:flutter_crud/provider/users.dart';
 import 'package:provider/provider.dart';
 
-const _tituloAppBar    = 'Cadastro de Usuário';
-const _lbTextNome      = 'Nome';
-const _lbTextEmail     = 'E-mail';
+const _tituloAppBar = 'Cadastro de Usuário';
+const _fieldId = 'id';
+const _fieldName = 'name';
+const _fieldEmail = 'email';
+const _fieldAvatarUrl = 'avatarUrl';
+
+const _lbTextId = 'id';
+const _lbTextNome = 'Nome';
+const _lbTextEmail = 'E-mail';
 const _lbTextAvatarUrl = 'Url do Avatar';
 
-class UserForm extends StatelessWidget {
+class UserForm extends StatefulWidget {
+  @override
+  _UserFormState createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
   final _form = GlobalKey<FormState>();
-  final Map<String, Object> _formData = {};
+
+  final Map<String, String> _formData = {};
+
+  void _loadFormData(User user) {
+    if(user != null) {
+      _formData[_fieldId] = user.id;
+      _formData[_fieldName] = user.name;
+      _formData[_fieldEmail] = user.email;
+      _formData[_fieldAvatarUrl] = user.avatarUrl;
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+
+    final User user = ModalRoute.of(context).settings.arguments as User;
+    _loadFormData(user);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text( _tituloAppBar ),
-        actions: <Widget> [
+        title: Text(_tituloAppBar),
+        actions: <Widget>[
           IconButton(
             icon: Icon(Icons.save),
             onPressed: () {
               final isValid = _form.currentState.validate();
-              if(isValid){
+              if (isValid) {
                 _form.currentState.save();
 
                 Provider.of<Users>(context, listen: false).put(
                   User(
-                    id: _formData['id'],
-                    name: _formData['name'],
-                    email: _formData['email'],
-                    avatarUrl: _formData['avatarUrl'],
+                    id: _formData[_fieldId],
+                    name: _formData[_fieldName],
+                    email: _formData[_fieldEmail],
+                    avatarUrl: _formData[_fieldAvatarUrl],
                   ),
                 );
 
                 Navigator.of(context).pop();
-              };
+              }
             },
           ),
         ],
       ),
       body: Padding(
         padding: EdgeInsets.all(32),
-        child: Form(
-          key: _form,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: _lbTextNome,
-                ),
-                validator: (value) {
-                  if(value == null || value.trim().isEmpty) {
-                    return 'O nome deve ser preenchido';
-                  }
+        child:
+          Form(
+            key: _form,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  initialValue: _formData[_fieldName],
+                  decoration: InputDecoration(
+                    labelText: _lbTextNome,
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'O nome deve ser preenchido';
+                    }
 
-                  if(value.trim().length < 3) {
-                    return 'Nome deve ter no mínimo 3 letras.';
-                  }
-                },
-                onSaved: (value) => _formData['name'] = value,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: _lbTextEmail,
+                    if (value.trim().length < 3) {
+                      return 'Nome deve ter no mínimo 3 letras.';
+                    }
+
+                    return null;
+                  },
+                  onSaved: (value) => _formData[_fieldName] = value,
                 ),
-                onSaved: (value) => _formData['email'] = value,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: _lbTextAvatarUrl,
+                TextFormField(
+                  initialValue: _formData[_fieldEmail],
+                  decoration: InputDecoration(
+                    labelText: _lbTextEmail,
+                  ),
+                  onSaved: (value) => _formData[_fieldEmail] = value,
                 ),
-                onSaved: (value) => _formData['avatarUrl'] = value,
-              ),
-            ],
-          )
+                TextFormField(
+                  initialValue: _formData[_fieldAvatarUrl],
+                  decoration: InputDecoration(
+                    labelText: _lbTextAvatarUrl,
+                  ),
+                  onSaved: (value) => _formData[_fieldAvatarUrl] = value,
+                ),
+              ],
+            ),
         ),
       ),
     );
